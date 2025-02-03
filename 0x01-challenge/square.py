cf71bd8f857462aff9486c08fe7da089dfab4d64
jmodhiambo <jmodhiambo9346@gmail.com>
2025-02-03 04:15:46 +0300

    My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-"""This module finds the area and perimeter of a square"""
+"""This class find the area and perimeter of a square"""
 
 
-class Square:
+class square():
     """Class representing a square"""
 
-    def __init__(self, size):
-        """Initialize the square with size"""
-        self.size = size  # A square should have equal width and height
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the square with width and height"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
-        """Calculate the area of the square"""
-        return self.size * self.size
+        """ Area of the square """
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
         """Calculate the perimeter of the square"""
-        return 4 * self.size
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """String representation of the square"""
-        return "{}/{}".format(self.size, self.size)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = Square(12)
+
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
